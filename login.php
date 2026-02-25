144304c6f4dafd55c239a2b30f9955237d788e27
KOCTUK1 <koctuk@rollins.edu>
2026-02-24 22:32:51 -0500

    Update login.php
    fixed some errors in the login page

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
 
 		<form method = "POST">
 			Username: <input type = "text" name = "username" required><br><br>
-			Password: <input type = "text" name = "password" required><br><br>\
+			Password: <input type = "text" name = "password" required><br><br>
 			<button type = "submit">Login</button>
 		</form>
 
